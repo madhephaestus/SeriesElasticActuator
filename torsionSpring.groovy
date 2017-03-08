9f86c6886168693c5d1980ac4fadce2ad2032564
harrington <harrington@AK120DNUC-AV.admin.wpi.edu>
2017-03-08 16:03:11 -0500

    torsion spring loaded from vitamin data

--- torsionSpring.groovy
+++ torsionSpring.groovy
@@ -1,1 +1,51 @@
-//Your code here
+import eu.mihosoft.vrl.v3d.parametrics.*;
+import com.neuronrobotics.bowlerstudio.vitamins.Vitamins;
+
+import javafx.scene.paint.Color;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import eu.mihosoft.vrl.v3d.parametrics.*;
+CSG getNut(){
+	String type= "torsionSpring"
+	if(args==null)
+		args=["Torsion-9271K621"]
+	StringParameter size = new StringParameter(	type+" Default",
+										args.get(0),
+										Vitamins.listVitaminSizes(type))
+	//println "Database loaded "+database
+	HashMap<String,Object> config = Vitamins.getConfiguration( type,size.getStrValue())
+	double height = config.wireDiameter * config.numOfCoils
+	
+	CSG core  =new Cylinder(config.od/2,config.od/2,height,(int)30).toCSG() // a one line Cylinder
+				.difference(new Cylinder(config.id/2,config.id/2,height,(int)30).toCSG())
+	CSG leg = new Cube(config.legLength, config.wireDiameter,config.wireDiameter).toCSG()
+				.toZMin()
+				.toXMin()
+				.toYMin()
+				.movey(-config.od/2)
+	
+
+	core = core.union(leg)
+			 .union(leg
+			 		.toZMax()
+			 		.movey(config.od-config.wireDiameter)
+			 		.rotz(-config.deflectionAngle)
+			 		.movez(height)
+			 		)
+	
+	return core
+		.setParameter(size)
+		.setRegenerate({getNut()})
+}
+return getNut()
