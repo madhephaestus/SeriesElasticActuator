f8b2eb228704329e417f38df898064cd260ce9f2
harrington <harrington@AK120DNUC-AV.admin.wpi.edu>
2017-03-08 14:52:09 -0500

    adding the printer offsets

--- encoderBoard.groovy
+++ encoderBoard.groovy
@@ -1,7 +1,10 @@
 import com.neuronrobotics.bowlerstudio.vitamins.Vitamins;
 import eu.mihosoft.vrl.v3d.parametrics.*;
 
-double magnetDiameter =6.0
+LengthParameter printerOffset 		= new LengthParameter("printerOffset",0.5,[1.2,0])
+HashMap<String, Object>  bearingData = Vitamins.getConfiguration("ballBearing","608zz")
+
+double magnetDiameter =6.0 - printerOffset.getMM()
 double magnetThickness = 2.5
 double magnetOffset =1.5
 double mountHoleRadius = 2.0/2
@@ -11,17 +14,15 @@ double boardLong = 28
 double boardShort = 22
 double cornerOffset = (boardShort-(chipToLongSide*2))/2
 double bearingOffset = magnetThickness+magnetOffset+1
-LengthParameter printerOffset 		= new LengthParameter("printerOffset",0.5,[1.2,0])
+double bearingHole =  bearingData.innerDiameter
+double bearingHoleWithOffset =(bearingHole - printerOffset.getMM())/2
 CSG bearing = Vitamins.get("ballBearing","608zz")
 			.movez(bearingOffset)
-HashMap<String, Object>  bearingData = Vitamins.getConfiguration("ballBearing","608zz")
-//println bearingData
-double bearingHole =  bearingData.innerDiameter
 
 CSG bearingCutterSlot = new Cube(bearingHole,bearingHole,bearingOffset +bearingData.width).toCSG()
 CSG bearingCutterSlotHole = new Cube(bearingHole*2/3,bearingHole,bearingOffset +bearingData.width).toCSG()
 						.movez(1)
-CSG bearingHoleInner = new Cylinder(bearingHole/2,bearingHole/2,bearingOffset +bearingData.width,(int)30).toCSG() // a one line Cylinder
+CSG bearingHoleInner = new Cylinder(bearingHoleWithOffset,bearingHoleWithOffset,bearingOffset +bearingData.width,(int)30).toCSG() // a one line Cylinder
 CSG bearingHoleOuter = new Cylinder(bearingHole/2+2,bearingHole/2+2,bearingOffset +bearingData.width,(int)30).toCSG() // a one line Cylinder
 					.difference(bearingHoleInner)
 
