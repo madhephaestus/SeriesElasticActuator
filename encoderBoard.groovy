59c23ae57f0c590d35ffa684ffc10084f2e42f00
harrington <harrington@AK120DNUC-AV.admin.wpi.edu>
2017-03-08 14:13:42 -0500

    added a bearing

--- encoderBoard.groovy
+++ encoderBoard.groovy
@@ -1,1 +1,48 @@
-//Your code here
+import com.neuronrobotics.bowlerstudio.vitamins.Vitamins;
+
+double magnetDiameter =6.0
+double magnetThickness = 2.5
+double magnetOffset =1.5
+double mountHoleRadius = 2.0/2
+double chipToShortside = 5.5
+double chipToLongSide  = 9.0
+double boardLong = 28
+double boardShort = 22
+double cornerOffset = (boardShort-(chipToLongSide*2))/2
+double bearingOffset = magnetThickness+magnetOffset+1
+
+CSG bearing = Vitamins.get("ballBearing","608zz")
+			.movez(bearingOffset)
+
+
+CSG magnet =new Cylinder(magnetDiameter/2,magnetDiameter/2,magnetThickness+magnetOffset,(int)30).toCSG() // a one line Cylinder
+
+CSG board = new Cube(	boardShort,// X dimention
+			boardLong,// Y dimention
+			1//  Z dimention
+			).toCSG()
+			.toZMax()
+			.toYMin()
+			.movey(-chipToShortside-cornerOffset)
+CSG bolt =new Cylinder(mountHoleRadius,mountHoleRadius,10,(int)30).toCSG() // a one line Cylinder
+						.movez(-5)
+
+board=board.union(bolt
+				.movex(chipToLongSide)
+				.movey(chipToShortside)
+				)
+		.union(bolt
+				.movex(-chipToLongSide)
+				.movey(chipToShortside)
+				)
+		.union(bolt
+				.movex(chipToLongSide)
+				.movey(-chipToShortside)
+				)
+		.union(bolt
+				.movex(-chipToLongSide)
+				.movey(-chipToShortside)
+				)
+		.union(magnet)
+		.union(bearing)
+return [board]
