ff86f1bfdde639d6196f54eee5857e5949d28616
Kevin Harrington <mad.hephaestus@gmail.com>
2017-05-09 14:39:50 -0400

    adding load cell components

--- loadCell.groovy
+++ loadCell.groovy
@@ -1,8 +1,28 @@
 //Your code here
 LengthParameter printerOffset		= new LengthParameter("printerOffset",0.5,[2,0.001])
-double barWidth = 14.5
+double barWidth = 12.7
+double nubWidth = 14.5
+double holeRad = 9.5/2.0
+double holeOffsetEdgeToEdge = 16.5
+
+CSG  loadHole =new Cylinder(holeRad,holeRad,nubWidth*2,(int)20).toCSG() // a one line Cylinder
+				.movez(-nubWidth)
+loadHole=loadHole
+		.toXMax()
+		.movex(holeOffsetEdgeToEdge/2)
+		.union(loadHole
+		.toXMin()
+		.movex(-holeOffsetEdgeToEdge/2))
+
+
 CSG bar = new Cube(80,barWidth,barWidth).toCSG()
-				.toXMin()
+				
+CSG nub = new Cube(30,nubWidth,nubWidth).toCSG()
+				
+bar=bar.union(nub)
+		.difference(loadHole)
+		.toXMin()
+				
 CSG baseBolt =Vitamins.get("capScrew","M5");
 CSG endBolt =Vitamins.get("capScrew","M4");
 baseBolt=baseBolt.union(baseBolt.movex(15))
