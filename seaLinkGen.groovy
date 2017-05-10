31f7d5b6cf9ee93e9f4834d05af1f8ad7d958a8b
Kevin Harrington <mad.hephaestus@gmail.com>
2017-05-10 11:31:04 -0400

    fixing imports

--- seaLinkGen.groovy
+++ seaLinkGen.groovy
@@ -1,4 +1,15 @@
+import com.neuronrobotics.bowlerstudio.creature.ICadGenerator;
+import com.neuronrobotics.bowlerstudio.creature.CreatureLab;
+import org.apache.commons.io.IOUtils;
+import com.neuronrobotics.bowlerstudio.vitamins.*;
+import eu.mihosoft.vrl.v3d.parametrics.*;
+import com.neuronrobotics.bowlerstudio.vitamins.Vitamins;
+import javafx.scene.paint.Color;
+import eu.mihosoft.vrl.v3d.Transform;
+import com.neuronrobotics.bowlerstudio.physics.TransformFactory;
+import eu.mihosoft.vrl.v3d.Transform;
 import javafx.scene.transform.Affine;
+
 
 Vitamins.setGitRepoDatabase("https://github.com/madhephaestus/Hardware-Dimensions.git")
 CSGDatabase.clear()
