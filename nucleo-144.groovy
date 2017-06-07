ea5e37bf6790371fbfd469515c1571574631563e
Kevin Harrington <mad.hephaestus@gmail.com>
2017-06-07 11:35:47 -0400

    returning both the board and the holder

--- nucleo-144.groovy
+++ nucleo-144.groovy
@@ -1,1 +1,47 @@
-//Your code here
+double holeDiam = 3.2
+double standoff =10
+CSG boltHole =new Cylinder(holeDiam/2,holeDiam/2,standoff*2,(int)30).toCSG()
+			.toXMin()
+			.toYMin()
+			.movez(-standoff/2)
+double standoffThickness = 2
+CSG standoffShaft =new Cylinder((holeDiam/2)+standoffThickness,(holeDiam/2)+standoffThickness,standoff,(int)30).toCSG()
+			.toXMin()
+			.toYMin()
+CSG standoffs = CSG.unionAll([
+			standoffShaft
+				.movey(9.0-standoffThickness)
+				.movex(26.3-standoffThickness),
+			standoffShaft
+				.movey(57.4-standoffThickness)
+				.movex(27.7-standoffThickness),
+			standoffShaft
+				.movey(56.6-standoffThickness)
+				.movex(103.0-standoffThickness),
+			standoffShaft
+				.movey(10.7-standoffThickness)
+				.movex(103.0-standoffThickness)
+			])					
+CSG bolts = CSG.unionAll([
+			boltHole
+				.movey(9.0)
+				.movex(26.3),
+			boltHole
+				.movey(57.4)
+				.movex(27.7),
+			boltHole
+				.movey(56.6)
+				.movex(103.0),
+			boltHole
+				.movey(10.7)
+				.movex(103.0)
+			])
+CSG board = new Cube(133.5,70,1.6).toCSG()
+			.toXMin()
+			.toYMin()
+			.movez(standoff)
+CSG finalboard=board.union(bolts)
+CSG bottom = board.toZMin()
+			.union(standoffs)
+			.difference(bolts)
+return [finalboard,bottom]
