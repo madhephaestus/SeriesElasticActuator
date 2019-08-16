6cf6198a5fb268d3e7c4a903dd1368f7ef86a9be
Kevin Harrington <mad.hephaestus@gmail.com>
2019-08-16 17:16:40 -0400

    removed recoursive call

--- LinkedGearedCadGen.groovy
+++ LinkedGearedCadGen.groovy
@@ -1,17 +1,3 @@
-
-return ScriptingEngine.gitScriptRun(
-            "https://github.com/madhephaestus/SeriesElasticActuator.git", // git location of the library
-            "LinkedGearedCadGen.groovy" , // file to load
-            // Parameters passed to the funcetion
-            [	  [36,// Number of teeth gear a link 0
-	            84],// Number of teeth gear b link 0
-	            [36,// Number of teeth gear a link 1
-	            84],// Number of teeth gear b link 1
-	            [36,// Number of teeth gear a link 2
-	            84],// Number of teeth gear b link 2
-            ]
-            )
-
 import com.neuronrobotics.bowlerstudio.creature.ICadGenerator;
 
 import org.apache.commons.io.IOUtils;
