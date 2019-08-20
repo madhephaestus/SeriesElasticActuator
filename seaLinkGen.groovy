9e0de6e75e36e089dcb3227f494ac2929afb15ca
rgbenasutti <rgbenasutti@fi103-03.mshome.net>
2019-08-20 15:08:46 -0400

    use correct shaft types

--- seaLinkGen.groovy
+++ seaLinkGen.groovy
@@ -1,13 +1,13 @@
 return ScriptingEngine.gitScriptRun(
-            "https://github.com/NotOctogonapus/SeriesElasticActuator.git", // git location of the library
+            "https://github.com/madhephaestus/SeriesElasticActuator.git", // git location of the library
             "LinkedGearedCadGen.groovy" , // file to load
             // Parameters passed to the funcetion
             [	  [36,// Number of teeth gear a link 0
-	            84],// Number of teeth gear b link 0
-	            [60,// Number of teeth gear a link 1
-	            60],// Number of teeth gear b link 1
+	            108],// Number of teeth gear b link 0
+	            [36,// Number of teeth gear a link 1
+	            108],// Number of teeth gear b link 1
 	            [36,// Number of teeth gear a link 2
-	            84],// Number of teeth gear b link 2
+	            108],// Number of teeth gear b link 2
             ]
             )
 
