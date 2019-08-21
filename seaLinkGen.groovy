21d43bcb6a0c65c69967b20f285854d607807216
rgbenasutti <rgbenasutti@FI103-13.admin.wpi.edu>
2019-08-21 13:45:47 -0400

    reduce driven gear size

--- seaLinkGen.groovy
+++ seaLinkGen.groovy
@@ -3,11 +3,11 @@ return ScriptingEngine.gitScriptRun(
             "LinkedGearedCadGen.groovy" , // file to load
             // Parameters passed to the funcetion
             [	  [36,// Number of teeth gear a link 0
-	            108],// Number of teeth gear b link 0
+	            42],// Number of teeth gear b link 0
 	            [36,// Number of teeth gear a link 1
-	            108],// Number of teeth gear b link 1
+	            42],// Number of teeth gear b link 1
 	            [36,// Number of teeth gear a link 2
-	            108],// Number of teeth gear b link 2
+	            42],// Number of teeth gear b link 2
             ]
             )
 
